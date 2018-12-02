b3ae59418e14e951f31e1e72bcd163d0b6175e25
DaniloGN <dan_guarizzo@hotmail.com>
2018-12-02 21:46:32 -0200

    cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+var cors = require('cors')
 app = express(),
 port = process.env.PORT || 3000,
 mongoose = require('mongoose'),
@@ -10,10 +11,15 @@ mongoose.Promise = global.Promise;
 uri = "mongodb://uhelsexypctpdas:V9BlhKt57hOcpjZ6sXzz@bqi740piavkunk4-mongodb.services.clever-cloud.com:27017/bqi740piavkunk4";
 mongoose.connect(uri);
 
-
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var routes = require('./api/routes/iSteamRoutes'); //importing route
 routes(app); //register the route
